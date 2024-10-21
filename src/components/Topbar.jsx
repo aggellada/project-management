3600a9c28b64b424696a89b946d3703ca4ebf719
anjogellada <anjogellada@gmail.com>
2024-10-21 19:22:30 +0800

    edited

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,9 +1,6 @@
 export default function Topbar({ currentDate }) {
   return (
     <div className="top_bar">
-      <div>
-        <h1>Hi, Alejandro!</h1>
-      </div>
       <div className="date">
         <p>{currentDate}</p>
       </div>
